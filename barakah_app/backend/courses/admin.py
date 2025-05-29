66f7b4c5fc277808798efa1557e981d6b6708faf
Nur Jamiludin <nj.ramadhan.mechatronics@gmail.com>
2025-05-29 20:04:58 +0700

    update admin page of Course

--- barakah_app/backend/courses/admin.py
+++ barakah_app/backend/courses/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import Course
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
 
-@admin.register(Course)
+class CourseAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget())  # Use CKEditorWidget for the article field
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'is_featured', 'is_active', 'price')
     list_filter = ('category', 'is_featured', 'is_active')
     search_fields = ('title', 'description')
-    date_hierarchy = 'created_at'  # Add a date filter for the deadline
+    date_hierarchy = 'created_at' 
+    form = CourseAdminForm  
+
+admin.site.register(Course, CourseAdmin)
