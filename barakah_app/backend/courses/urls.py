24611a1cf76256f5e7c88a8d845d68d646ba2247
galanmild <galanmildpro@gmail.com>
2026-03-13 13:18:54 +0700

    Update urls.py

--- barakah_app/backend/courses/urls.py
+++ barakah_app/backend/courses/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import (
+    CourseViewSet, CourseDetailViewSet, CourseEnrollmentViewSet, 
     CourseMaterialViewSet, CoursePaymentConfirmationView, 
     UserCourseProgressViewSet, CertificateRequestViewSet, AdminCourseViewSet,
     CourseShareView
