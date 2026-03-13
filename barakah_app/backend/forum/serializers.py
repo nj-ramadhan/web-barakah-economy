3387fb33fc4490cec9d772ed7303f82f2b9c598e
galanmild <galanmildpro@gmail.com>
2026-03-14 05:38:14 +0700

    Update serializers.py

--- barakah_app/backend/forum/serializers.py
+++ barakah_app/backend/forum/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Thread, Reply
+from .models import Thread, Reply, MentionNotification
 from accounts.models import User
 
 class AuthorSerializer(serializers.ModelSerializer):
