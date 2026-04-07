e3751a96c1150f0ddff58699e4088188efc90bd3
galanmild <galanmildpro@gmail.com>
2026-04-07 18:02:29 +0700

    Update EventSubmissionPage.js

--- barakah_app/frontend/src/pages/EventSubmissionPage.js
+++ barakah_app/frontend/src/pages/EventSubmissionPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import Header from '../components/layout/Header';
