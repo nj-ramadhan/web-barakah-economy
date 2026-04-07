0ff341b8b863307fb8c2b15008fae002ee9fb4f9
galanmild <galanmildpro@gmail.com>
2026-04-07 18:21:02 +0700

    Update DashboardMyEventsPage.js

--- barakah_app/frontend/src/pages/DashboardMyEventsPage.js
+++ barakah_app/frontend/src/pages/DashboardMyEventsPage.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
 import NavigationButton from '../components/layout/Navigation';
 import { Helmet } from 'react-helmet';
 
@@ -177,7 +176,6 @@ const DashboardMyEventsPage = () => {
             </main>
 
             <NavigationButton />
-            <Footer />
         </div>
     );
 };
