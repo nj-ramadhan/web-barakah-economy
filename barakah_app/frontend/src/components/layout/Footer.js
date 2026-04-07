34c0cd8ec1680cd918941aa4a5ef41646f890b77
galanmild <galanmildpro@gmail.com>
2026-04-07 18:13:12 +0700

    Update Footer.js

--- barakah_app/frontend/src/components/layout/Footer.js
+++ barakah_app/frontend/src/components/layout/Footer.js
@@ -4,14 +4,14 @@ import '../../styles/Footer.css';
 
 const Footer = () => {
   return (
-    <Footer className="footer">
+    <footer className="footer">
       <div className="container">
         <div className="flex items-center">
           <img src="/images/logo.png" alt="YPMN" className="logo" />
-          <span className="title">YPMN PEDULI</span>
+          <span className="title">BARAKAH ECONOMY</span>
         </div>
       </div>
-    </Footer>
+    </footer>
   );
 };
 
