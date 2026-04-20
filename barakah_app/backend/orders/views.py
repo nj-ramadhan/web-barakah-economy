1097f49112b89d5a473092ffc56b0d6fb735b667
galanmild <galanmildpro@gmail.com>
2026-04-20 13:02:33 +0700

    Update views.py

--- barakah_app/backend/orders/views.py
+++ barakah_app/backend/orders/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
 from django.shortcuts import get_object_or_404
 from .models import Order, OrderItem
 from carts.models import Cart # Assuming you have a Cart and CartItem model
