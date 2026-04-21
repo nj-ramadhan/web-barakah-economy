a6e07e71d1c98b037c0152d09a291d4cd4169b6e
galanmild <galanmildpro@gmail.com>
2026-04-21 16:21:59 +0700

    Update views.py

--- barakah_app/backend/orders/views.py
+++ barakah_app/backend/orders/views.py
@@ -1,5 +1,6 @@
 # orders/views.py
-from rest_framework import generics
+from rest_framework import generics, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
