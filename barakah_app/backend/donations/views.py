c4e49468ba2226bd9c4a17fabd858d53c4e8eeba
galanmild <galanmildpro@gmail.com>
2026-04-26 21:11:04 +0700

    Update views.py

--- barakah_app/backend/donations/views.py
+++ barakah_app/backend/donations/views.py
@@ -1,6 +1,7 @@
 # donations/views.py
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
