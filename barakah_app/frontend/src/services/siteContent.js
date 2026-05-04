f19dfadef20347b3a6475ee6d8b259aee5c1a189
galanmild <galanmildpro@gmail.com>
2026-05-04 15:47:50 +0700

    fix: change site_content to site-content in API paths

--- barakah_app/frontend/src/services/siteContent.js
+++ barakah_app/frontend/src/services/siteContent.js
@@ -1,20 +1,20 @@
 import api from './api';
 
 const siteContentService = {
-  getPartners: () => api.get('/site_content/partners/'),
-  getTestimonials: () => api.get('/site_content/testimonials/'),
-  getActivities: () => api.get('/site_content/activities/'),
-  getAboutUs: () => api.get('/site_content/about-us/'),
-  getAnnouncements: () => api.get('/site_content/announcements/'),
+  getPartners: () => api.get('/site-content/partners/'),
+  getTestimonials: () => api.get('/site-content/testimonials/'),
+  getActivities: () => api.get('/site-content/activities/'),
+  getAboutUs: () => api.get('/site-content/about-us/'),
+  getAnnouncements: () => api.get('/site-content/announcements/'),
   
   // Admin actions
-  createAnnouncement: (data) => api.post('/site_content/announcements/', data, {
+  createAnnouncement: (data) => api.post('/site-content/announcements/', data, {
     headers: { 'Content-Type': 'multipart/form-data' }
   }),
-  updateAnnouncement: (id, data) => api.patch(`/site_content/announcements/${id}/`, data, {
+  updateAnnouncement: (id, data) => api.patch(`/site-content/announcements/${id}/`, data, {
     headers: { 'Content-Type': 'multipart/form-data' }
   }),
-  deleteAnnouncement: (id) => api.delete(`/site_content/announcements/${id}/`),
+  deleteAnnouncement: (id) => api.delete(`/site-content/announcements/${id}/`),
 };
 
 export default siteContentService;
