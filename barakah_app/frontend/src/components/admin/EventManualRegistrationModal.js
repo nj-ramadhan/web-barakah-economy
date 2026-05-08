199325d9eb5709c39e0b76a72d241af879a4e738
galanmild <galanmildpro@gmail.com>
2026-05-08 16:42:43 +0700

    Update EventManualRegistrationModal.js

--- barakah_app/frontend/src/components/admin/EventManualRegistrationModal.js
+++ barakah_app/frontend/src/components/admin/EventManualRegistrationModal.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
+import axios from 'axios';
 import { manualRegisterParticipant, getAvailableUsers, bulkManualRegister } from '../../services/eventApi';
 import UserSelectionOverlay from './UserSelectionOverlay';
 
