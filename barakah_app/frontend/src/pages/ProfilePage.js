916c0998e4f8b4e94762be473d1eb54d07a35605
galanmild <galanmildpro@gmail.com>
2026-08-15 03:51:40 +0700

    Update ProfilePage.js

--- barakah_app/frontend/src/pages/ProfilePage.js
+++ barakah_app/frontend/src/pages/ProfilePage.js
@@ -1472,6 +1472,9 @@ const ProfilePage = () => {
                             </div>
                         </form>
                     </div>
+                </div>
+            )}
+
             {/* Modal Riwayat Mutasi Saldo BAE */}
             {showWalletModal && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black/60 backdrop-blur-sm animate-in fade-in duration-200">
